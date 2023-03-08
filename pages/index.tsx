953ec907064499c7c2de84aaa7d1eeca8ee59e06
moez-rd <sloe9ie@gmail.com>
2023-03-08 13:24:58 +0700

    Add effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,11 @@ import * as c from "@/lib/constants";
 import * as f from "@/lib/functions";
 import { Divider, ProfileCard, ReportCountCard } from "@/components";
 import Link from "next/link";
+import { particles } from "@/lib/particles";
+import { useCallback } from "react";
+import Particles from "react-particles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
 
 const notion = new Client({ auth: process.env.NOTION_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID ?? "";
@@ -17,6 +22,22 @@ export default function Home({ notionWorkTasks, notionOrganizers }: any) {
   const mulStaff = f.filterOrganizer(notionOrganizers, c.MUL_STAFF);
   const pubStaff = f.filterOrganizer(notionOrganizers, c.PUB_STAFF);
 
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+
   return (
     <>
       <Head>
@@ -28,32 +49,40 @@ export default function Home({ notionWorkTasks, notionOrganizers }: any) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <header className="relative bg-blue-500 py-40 px-4 text-center">
+      <Particles
+        id="tsparticles"
+        className="absolute -z-10"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particles}
+      />
+      <div className="relative -z-20">
+        <header className="bg-blue-500 py-40 px-4 text-center">
+          <svg
+            className="mx-auto w-16 fill-blue-900/80 md:w-20"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+          >
+            <path d="M192 64L160 0H128L96 64 64 0H48C21.5 0 0 21.5 0 48V256H384V48c0-26.5-21.5-48-48-48H224L192 64zM0 288v32c0 35.3 28.7 64 64 64h64v64c0 35.3 28.7 64 64 64s64-28.7 64-64V384h64c35.3 0 64-28.7 64-64V288H0zM192 464c-8.8 0-16-7.2-16-16s7.2-16 16-16s16 7.2 16 16s-7.2 16-16 16z" />
+          </svg>
+          <p className="mt-8 text-sm text-blue-100 md:text-base">
+            <span className="tracking-[1.2rem]">-DINAS</span>-
+          </p>
+          <h1 className="mt-1 text-4xl font-black text-blue-100 sm:mt-4 md:text-5xl">
+            Informasi dan Komunikasi
+          </h1>
+          <p className="font-light text-blue-300">
+            Himpunan Mahasiswa Sistem Komputer Universitas Sriwijaya
+          </p>
+        </header>
         <svg
-          className="mx-auto w-16 fill-blue-900/80 md:w-20"
+          className="-translate-y-9 fill-blue-500 sm:-translate-y-16 lg:-translate-y-20 xl:-translate-y-28 2xl:-translate-y-36"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 384 512"
+          viewBox="0 0 1440 320"
         >
-          <path d="M192 64L160 0H128L96 64 64 0H48C21.5 0 0 21.5 0 48V256H384V48c0-26.5-21.5-48-48-48H224L192 64zM0 288v32c0 35.3 28.7 64 64 64h64v64c0 35.3 28.7 64 64 64s64-28.7 64-64V384h64c35.3 0 64-28.7 64-64V288H0zM192 464c-8.8 0-16-7.2-16-16s7.2-16 16-16s16 7.2 16 16s-7.2 16-16 16z" />
+          <path d="M0,224L48,208C96,192,192,160,288,170.7C384,181,480,235,576,250.7C672,267,768,245,864,213.3C960,181,1056,139,1152,144C1248,149,1344,203,1392,229.3L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
         </svg>
-        <p className="mt-8 text-sm text-blue-100 md:text-base">
-          <span className="tracking-[1.2rem]">-DINAS</span>-
-        </p>
-        <h1 className="mt-1 text-4xl font-black text-blue-100 sm:mt-4 md:text-5xl">
-          Informasi dan Komunikasi
-        </h1>
-        <p className="font-light text-blue-300">
-          Himpunan Mahasiswa Sistem Komputer Universitas Sriwijaya
-        </p>
-      </header>
-      <svg
-        className="-z-10 -translate-y-9 fill-blue-500 sm:-translate-y-16 lg:-translate-y-20 xl:-translate-y-28 2xl:-translate-y-36"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-      >
-        <path d="M0,224L48,208C96,192,192,160,288,170.7C384,181,480,235,576,250.7C672,267,768,245,864,213.3C960,181,1056,139,1152,144C1248,149,1344,203,1392,229.3L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
-      </svg>
+      </div>
       <div className="mx-2 max-w-6xl sm:mx-4 xl:mx-auto">
         <main className="mt-20 space-y-20">
           <Divider />
