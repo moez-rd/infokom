16db027ff47f3d900cef085f4799ca5196b942f8
moez-rd <sloe9ie@gmail.com>
2023-03-24 21:30:53 +0700

    Add terms and conditions

--- pages/media-partnership.tsx
+++ pages/media-partnership.tsx
@@ -4,6 +4,15 @@ import * as c from "@/lib/constants";
 import { Divider } from "@/components";
 
 export default function MediaPartnership({ notionWorkTasks }: any) {
+  const termsAndConditions = [
+    "Pihak yang meminta media partnership dengan Himasisko wajib memfollow instagram @himasiskounsri minimal 15 orang.",
+    "Publikasi yang dilakukan berupa kegiatan kemahasiswaan yang berkaitan dengan penalaran, keilmiahan, seni, olahraga, atau lomba.",
+    "Publikasi yang Himasisko lakukan berupa publikasi poster kegiatan atau event.",
+    "Poster event akan dibagikan sebanyak satu kali atau lebih sesuai dengan kesepakatan oleh Himasisko.",
+    "Poster event akan dibagikan hanya melalui Instaragam Himasisko @himasiskounsri",
+    "Media partnership wajib mencantumkan logo Himasisko sebagai logo partnership.",
+  ];
+
   return (
     <>
       <Head>
@@ -23,7 +32,7 @@ export default function MediaPartnership({ notionWorkTasks }: any) {
         </header>
         <Divider />
         <main className="m-10 mx-auto max-w-2xl text-center">
-          <div className="rounded border p-4">
+          <div className="rounded border py-4 px-8 sm:px-16">
             <h2 className="text-lg font-bold text-blue-500">
               Ingin Bekerja Sama dengan Himasisko?
             </h2>
@@ -33,11 +42,21 @@ export default function MediaPartnership({ notionWorkTasks }: any) {
                 Himasisko untuk advertising dan publikasi media promosi melalui
                 sosial media di akun Himasisko.
               </p>
-              <p className="mt-4">
+              <div>
+                <h2 className="mt-8 text-lg font-bold text-blue-500">
+                  Syarat dan Ketentuan
+                </h2>
+                <ul className="list-outside list-decimal text-left">
+                  {termsAndConditions.map((termAndCondition, key) => (
+                    <li key={key}>{termAndCondition}</li>
+                  ))}
+                </ul>
+              </div>
+              <p className="mt-8">
                 Silakan menghubungi contact person berikut untuk informasi lebih
                 lanjut.
               </p>
-              <div className="mt-4 flex space-y-2 sm:space-y-0 sm:flex-row flex-col justify-center space-x-1">
+              <div className="mt-4 flex flex-col justify-center space-y-2 space-x-1 sm:flex-row sm:space-y-0">
                 <div>
                   <a
                     className="inline-block rounded bg-blue-500 px-2 py-1 text-blue-100 hover:bg-blue-400"
