b4eba8fb6efbc327a7b1e53695daea792ee99f4d
Rahmat <slow9ie@gmail.com>
2023-12-19 12:54:11 +0700

    fix bug

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3.us-west-2.amazonaws.com",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
       },
     ],
   },
